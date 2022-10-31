de65c527944716c248b53ba5776f7115457eefe2
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2022-10-31 19:54:36 +0500

    done

---
+++ src/index.js
@@ -0,0 +1,16 @@
+// JavaScript HTML DOM Elements
+const toggleBtn = document.getElementById("toggle-btn");
+
+
+// Add Event Listener
+toggleBtn.addEventListener("click", toggleSwitchBtn);
+
+// Functions
+function toggleSwitchBtn() {
+  if(toggleBtn.src.indexOf('toggle-btn-dark.svg')!=-1){
+     toggleBtn.src = "./assets/images/icons/toggle-btn-white.svg";
+  }else {
+    toggleBtn.src = "./assets/images/icons/toggle-btn-dark.svg";
+  }
+};
+                      
