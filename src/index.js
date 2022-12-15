6df89a11fa1c59177b281a8b9f943d2d5e809a94
SHakhzodTojiyev <tojiyevshakhzod@gmail.com>
2022-12-15 16:17:27 +0500

    done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 // JavaScript HTML DOM Elements
 const root = document.getElementById("root");
 const toggleBtn = document.getElementById("toggle-btn");
-
+const iconGit = document.getElementById("icon-git");
+const iconTwitter = document.getElementById("icon-twitter");
+const iconLinkedin = document.getElementById("icon-linkedin");
+const iconYoutube = document.getElementById("icon-youtube");
 
 // Add Event Listener
 toggleBtn.addEventListener("click", toggleSwitchBtn);
 
 // Functions
 function toggleSwitchBtn() {
-  if(toggleBtn.src.indexOf('toggle-btn-white.svg')!=-1){
-     toggleBtn.src = "./assets/images/toggle-btn-dark.svg";
-     root.classList.add("dark")
-  }else {
+  if (toggleBtn.src.indexOf("toggle-btn-dark.svg") != -1) {
     toggleBtn.src = "./assets/images/toggle-btn-white.svg";
-    root.classList.remove("dark")
+    iconGit.src = "./assets/images/icons/git-white.svg";
+    iconTwitter.src = "./assets/images/icons/twitter-white.svg";
+    iconLinkedin.src = "./assets/images/icons/linkedin-white.svg";
+    iconYoutube.src = "./assets/images/icons/youtube-white.svg";
+    root.classList.add("dark");
+  } else {
+    toggleBtn.src = "./assets/images/toggle-btn-dark.svg";
+    iconGit.src = "./assets/images/icons/git-dark.svg";
+    iconTwitter.src = "./assets/images/icons/twitter-dark.svg";
+    iconLinkedin.src = "./assets/images/icons/linkedin-dark.svg";
+    iconYoutube.src = "./assets/images/icons/youtube-dark.svg";
+    root.classList.remove("dark");
   }
-};
-               
+}
+
 console.log(root);
